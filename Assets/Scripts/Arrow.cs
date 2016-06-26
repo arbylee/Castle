4583f7255900986d14aac48004f9d3641c272a66
Richard Lee <rblee88@gmail.com>
2016-06-26 14:38:16 +1000

    Add particle effect when enemy is destroyed

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,16 +4,22 @@ using System.Collections;
 public class Arrow : MonoBehaviour {
 	private Rigidbody rb;
 	private Collider col;
+	private GameObject explosion;
 
 	void Awake() {
 		rb = GetComponent<Rigidbody> ();
 		col = GetComponent<Collider> ();
 	}
 
+	void Start(){
+		explosion = GameObject.FindGameObjectWithTag ("Explosion");
+	}
+
 	void OnTriggerEnter (Collider other) {
 		GameObject hitTarget = other.gameObject;
 		if (hitTarget.CompareTag ("Enemy")) {
 			Destroy(hitTarget.gameObject);
+			Instantiate (explosion, hitTarget.transform.position, Quaternion.identity);
 		} else {
 			this.transform.parent = other.gameObject.transform;
 			Destroy (rb);
